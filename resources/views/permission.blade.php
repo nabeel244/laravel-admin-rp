bfca9b46a3404be7d128e31242d2fae1bf2b23fe
nabeel244 <nabeelmuhammad244@gmail.com>
2021-07-30 00:43:11 +0500

    admin-child

--- resources/views/permission.blade.php
+++ resources/views/permission.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.main') 
+@extends('layouts.main')
 @section('title', 'Permission')
 @section('content')
     <!-- push external head elements to head -->
@@ -6,17 +6,17 @@
         <link rel="stylesheet" href="{{ asset('plugins/DataTables/datatables.min.css') }}">
         <link rel="stylesheet" href="{{ asset('plugins/select2/dist/css/select2.min.css') }}">
     @endpush
-    
-    
+
+    {{-- add document with push admin-child --}}
     <div class="container-fluid">
-    	<div class="page-header">
+        <div class="page-header">
             <div class="row align-items-end">
                 <div class="col-lg-8">
                     <div class="page-header-title">
                         <i class="ik ik-unlock bg-blue"></i>
                         <div class="d-inline">
-                            <h5>{{ __('Permissions')}}</h5>
-                            <span>{{ __('Define permissions of user')}}</span>
+                            <h5>{{ __('Permissions') }}</h5>
+                            <span>{{ __('Define permissions of user') }}</span>
                         </div>
                     </div>
                 </div>
@@ -27,7 +27,7 @@
                                 <a href="../index.html"><i class="ik ik-home"></i></a>
                             </li>
                             <li class="breadcrumb-item">
-                                <a href="#">{{ __('Permissions')}}</a>
+                                <a href="#">{{ __('Permissions') }}</a>
                             </li>
                         </ol>
                     </nav>
@@ -40,35 +40,40 @@
             <!-- end message area-->
             <!-- only those have manage_permission permission will get access -->
             @can('manage_permission')
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-header"><h3>{{ __('Add Permission')}}</h3></div>
-                    <div class="card-body">
-                        <form class="forms-sample" method="POST" action="{{url('permission/create')}}">
-                            @csrf
-                            <div class="row">
-                                <div class="col-sm-4">
-                                    <div class="form-group">
-                                        <label for="permission">{{ __('Permission')}}<span class="text-red">*</span></label>
-                                        <input type="text" class="form-control" id="permission" name="permission" placeholder="Permission Name" required>
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h3>{{ __('Add Permission') }}</h3>
+                        </div>
+                        <div class="card-body">
+                            <form class="forms-sample" method="POST" action="{{ url('permission/create') }}">
+                                @csrf
+                                <div class="row">
+                                    <div class="col-sm-4">
+                                        <div class="form-group">
+                                            <label for="permission">{{ __('Permission') }}<span
+                                                    class="text-red">*</span></label>
+                                            <input type="text" class="form-control" id="permission" name="permission"
+                                                placeholder="Permission Name" required>
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <div class="form-group">
+                                            <label for="exampleInputEmail3">{{ __('Assigned to Role') }} </label>
+                                            {!! Form::select('roles[]', $roles, null, ['class' => 'form-control select2', 'multiple' => 'multiple']) !!}
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-2">
+                                        <div class="form-group">
+                                            <button type="submit"
+                                                class="btn btn-primary btn-rounded">{{ __('Save') }}</button>
+                                        </div>
                                     </div>
                                 </div>
-                                <div class="col-sm-6">
-                                    <div class="form-group">
-                                        <label for="exampleInputEmail3">{{ __('Assigned to Role')}} </label>
-                                        {!! Form::select('roles[]', $roles, null,[ 'class'=>'form-control select2', 'multiple' => 'multiple']) !!}
-                                    </div>
-                                </div>
-                                <div class="col-sm-2">
-                                    <div class="form-group">
-                                        <button type="submit" class="btn btn-primary btn-rounded">{{ __('Save')}}</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </form>
+                            </form>
+                        </div>
                     </div>
                 </div>
-            </div>
             @endcan
         </div>
         <div class="row">
@@ -78,9 +83,9 @@
                         <table id="permission_table" class="table">
                             <thead>
                                 <tr>
-                                    <th>{{ __('Permission')}}</th>
-                                    <th>{{ __('Assigned Role')}}</th>
-                                    <th>{{ __('Action')}}</th>
+                                    <th>{{ __('Permission') }}</th>
+                                    <th>{{ __('Assigned Role') }}</th>
+                                    <th>{{ __('Action') }}</th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -93,10 +98,10 @@
     </div>
     <!-- push external js -->
     @push('script')
-    <script src="{{ asset('plugins/select2/dist/js/select2.min.js') }}"></script>
-    <script src="{{ asset('plugins/DataTables/datatables.min.js') }}"></script>
-    <script src="{{ asset('plugins/DataTables/Cell-edit/dataTables.cellEdit.js') }}"></script>
-    <!--server side permission table script-->
-    <script src="{{ asset('js/permission.js') }}"></script>
+        <script src="{{ asset('plugins/select2/dist/js/select2.min.js') }}"></script>
+        <script src="{{ asset('plugins/DataTables/datatables.min.js') }}"></script>
+        <script src="{{ asset('plugins/DataTables/Cell-edit/dataTables.cellEdit.js') }}"></script>
+        <!--server side permission table script-->
+        <script src="{{ asset('js/permission.js') }}"></script>
     @endpush
 @endsection
